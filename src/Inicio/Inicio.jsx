34c3052611626ee999030cb2266bf95e88e612c6
Ariel Ferreiro <ferreiroariel1@gmail.com>
2023-11-28 08:35:23 -0300

    correos integrados, pero sin estilo

--- src/Inicio/Inicio.jsx
+++ src/Inicio/Inicio.jsx
@@ -26,6 +26,27 @@ function Inicio() {
       </div>
       </div>
       
+       
+      <form action="https://formsubmit.co/719de6815d48ba2f1b923a4f1955b430" method="POST">
+            <div class="input-container">
+                <label for="form-name">Tu nombre*</label>
+                <input type="text" name="name"  required="" placeholder="Tu nombre"/>
+            </div>
+            
+            <div class="input-container">
+                <label for="form-email">Tu e-mail*</label>
+                <input type="email" name="email"  required="" placeholder="ejemplo@email.com"/>
+            </div>
+
+            <div class="input-container">
+                <label for="form-message">Tu mensaje</label>
+                <textarea name="message"  cols="20" rows="15" placeholder="Hola, mundo!"></textarea>
+            </div>
+
+           <button type="submit">Enviar</button>
+           <input type="hidden" name="_next" value="https://mi-portafolio-ariel-ferreiro.netlify.app/" />
+           <input type="hidden" name="_captcha" value="false" />
+        </form>
 
     </section>
   )
