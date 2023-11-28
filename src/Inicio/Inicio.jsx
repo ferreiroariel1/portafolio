b3c1e74619bdb630d3ba3b4900c31398ca7bfbde
Ariel Ferreiro <ferreiroariel1@gmail.com>
2023-11-28 17:51:17 -0300

    hola

--- src/Inicio/Inicio.jsx
+++ src/Inicio/Inicio.jsx
@@ -7,20 +7,6 @@ import cv from '../../public/pdf/cv.pdf'
 
 function Inicio() {
  
-  useEffect(() => {
-    const handleScroll = () => {
-      const scrollButton = document.getElementById('scroll-to-top');
-      if (scrollButton) scrollButton.style.display = window.scrollY > 20 || document.documentElement.scrollTop > 20 ? 'block' : 'none';
-    };
-  
-    window.addEventListener('scroll', handleScroll);
-  
-    return () => window.removeEventListener('scroll', handleScroll);
-  }, []);
-  
-  const scrollToTop = () => {
-    document.documentElement.scrollTop = 0;
-  };
    return (
     <section className={style.Inicio}>
 
@@ -40,32 +26,6 @@ function Inicio() {
         <a href={cv} download target="_blank" rel="noopener noreferrer">Descargar CV</a>
       </div>
       </div>
-      
-       
-      <form className={style.form} action="https://formsubmit.co/719de6815d48ba2f1b923a4f1955b430" method="POST">
-            <div class="input-container">
-                <label for="form-name">Tu nombre*</label>
-                <input type="text" name="name"  required="" placeholder="Tu nombre"/>
-            </div>
-            
-            <div class="input-container">
-                <label for="form-email">Tu e-mail*</label>
-                <input type="email" name="email"  required="" placeholder="ejemplo@email.com"/>
-            </div>
-
-            <div class="input-container">
-                <label for="form-message">Tu mensaje</label>
-                <textarea name="message"  cols="20" rows="15" placeholder="Hola, mundo!"></textarea>
-            </div>
-
-           <button type="submit">Enviar</button>
-           <input type="hidden" name="_next" value="https://mi-portafolio-ariel-ferreiro.netlify.app/" />
-           <input type="hidden" name="_captcha" value="false" />
-        </form>
-        
-        <button id="scroll-to-top" onClick={scrollToTop}>
-        ▲
-      </button>
     </section>
   )
 }
